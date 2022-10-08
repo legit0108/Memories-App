219a722143c7be2909307f58d7bf71b7cc49d9ed
legit0108 <legitgamer2345@gmail.com>
2022-10-08 22:26:07 +0530

    Implemented delete post, like post, code refactoring

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,13 +1,17 @@
+import {FETCH_ALL, CREATE, UPDATE, DELETE, LIKE} from '../constants/actionTypes';
+
 export default (posts = [], action) => {
     const type = action.type;
     const payload = action.payload;
 
-    if(type === 'FETCH_ALL'){
+    if(type === FETCH_ALL){
         return payload;
-    }else if(type === 'CREATE'){
+    }else if(type === CREATE){
         return [...posts, payload]
-    }else if(type === 'UPDATE'){
+    }else if(type === UPDATE || type === LIKE){
         return posts.map((post) => post._id === action.payload._id ? action.payload : post)
+    }else if(type === DELETE){
+        return posts.filter((post) => post._id !== action.payload)
     }
 
     return posts;
